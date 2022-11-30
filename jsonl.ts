0c32ba0caa146b85cf99f59adffe15ff8f2278c8
Kenta Hamanaka <kenta.hamanaka@gmail.com>
2022-11-30 12:39:14 +0900

    :sparkles: Add pick

--- jsonl.ts
+++ jsonl.ts
@@ -1,9 +1,13 @@
 #!/usr/bin/env -S deno run
 import { readLines } from "https://deno.land/std@0.166.0/io/mod.ts";
+import pick from "https://deno.land/x/lodash@4.17.15-es/pick.js";
+
+const keys = Deno.args;
 
 for await (const line of readLines(Deno.stdin)) {
   const d = JSON.parse(line);
   if (d) {
-    console.log(d);
+    if (keys.length > 0) console.log(pick(d, keys));
+    else console.log(d);
   }
 }
